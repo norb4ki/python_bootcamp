a3d8b10485821783b9926da4871bbfaba2e882d1
Vladimir Baraniuk <vladimirbaraniuk@gmail.com>
2025-10-15 20:50:39 +0300

    feat: add task for day#1

---
+++ 1-10/D1/index.py
@@ -0,0 +1,4 @@
+print('Welcome to the Band Name Generator')
+city = input('What is the name of the city you grew up in?\n')
+animal = input('What is the coolest animal in your opinion?\n')
+print(f'Please, welcome "{city} {animal}s"')
