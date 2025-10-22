b2baacdd467ae848e7ef1cf600378a295c7e559c
Vladimir Baraniuk <vladimirbaraniuk@gmail.com>
2025-10-22 09:11:34 +0300

    feat: add win condition

--- 1-10/Day #7 Hangman/main.py
+++ 1-10/Day #7 Hangman/main.py
@@ -1,11 +1,70 @@
 import random
 word_list = ['pen', 'pineapple', 'apple', ]
+stages = [r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 
 guessed_letters = []
 chosen_word = random.choice(word_list)
-guessed_word = ''
+word_length = len(chosen_word)
+game_over = False
+lives_left = 6
+placeholder = ''
 for step in range (len(chosen_word)):
-  guessed_word += '-'
+  placeholder += '-'
 
 
 def guess_letter():
@@ -27,25 +86,44 @@ def is_letter_valid(letter):
 def is_letter_in_word(letter):
   if letter in chosen_word:
     print('Right!\n')
-    update_word()
+    return True
   else:
     print('Wrong!\n')
+    return False
 
-def update_word():
-  global guessed_word
+def update_word(letter):
+  global placeholder
   new_word = ''
+
   for letter in chosen_word:
     if letter in guessed_letters:
       new_word += letter
     else:
       new_word += '-'
-  guessed_word = new_word
+  placeholder = new_word
 
-while not chosen_word == guessed_word:
-  print('Word to guess: ',guessed_word)
+while not game_over:
+  print(stages[lives_left])
+  print('Word to guess: ',placeholder)
   letter = guess_letter()
 
   if is_letter_valid(letter):
     guessed_letters.append(letter)
-    is_letter_in_word(letter)
+    
+    if is_letter_in_word(letter):
+      update_word(letter)
+    else:
+      lives_left -= 1
+      print(f'You have {lives_left} lives left\n')
+
+  if chosen_word == placeholder:
+    print('You won!\n' \
+    f'The word is: {chosen_word}')
+    game_over = True
   
+  if lives_left <= 0:
+    print(stages[lives_left])
+    print('You lose :(\n' \
+    f'The word is: {chosen_word}')
+    game_over = True
+    
