13d61be955535f1c6d8b8d5ad697da70554aeaf1
Vladimir Baraniuk <vladimirbaraniuk@gmail.com>
2025-10-26 10:02:57 +0200

    refactor: move a part of code to separate functions

--- 11-20/Day_#12_Scope/main.py
+++ 11-20/Day_#12_Scope/main.py
@@ -1,36 +1,49 @@
 from art import logo
 import random
 
+EASY_LEVEL_TURNS = 5
+HARD_LEVEL_TURNS = 5
+
+def is_answer_correct(guess, answer):
+  """Returns True is the guess was correct, False otherwise """
+  if guess > answer:
+    print("Too high.")
+    return False
+  elif guess < answer:
+    print("Too low.")
+    return False
+  else:
+    print(f'You got it! The answer is {answer}.')
+    return True
+
+def get_difficulty():
+  """Asks a user for a difficulty and returns corresponding amount of turns"""
+  difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+  while difficulty!= 'easy' and difficulty!= 'hard':
+    difficulty = input("You can choose only from 'easy' and 'hard': ").lower()
+
+  if difficulty == "easy":
+    turns = EASY_LEVEL_TURNS
+  else:
+    turns = HARD_LEVEL_TURNS
+  return turns
+
+
 print(logo)
 print("Welcome to the Number Guessing Game!\nI'm thinking of a number between 1 and 100.")
 
-secret_number = random.randint(1, 100)
-user_guess = 0
-guesses_left = 0
+answer = random.randint(1, 100)
+turns = get_difficulty()
 game_over = False
 
-difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
-while difficulty!= 'easy' and difficulty!= 'hard':
-  difficulty = input("You can choose only from 'easy' and 'hard': ").lower()
-
-if difficulty == "easy":
-  guesses_left = 10
-else:
-  guesses_left = 5
-
-while not game_over and guesses_left > 0:
-  print(f"You have {guesses_left} attempts to guess the number.")
+while not game_over and turns > 0:
+  print(f"You have {turns} attempts to guess the number.")
   guess = int(input("Take a guess: "))
-  guesses_left -= 1
+  turns -= 1
 
-  if guess > secret_number:
-    print("Too high.")
-  elif guess < secret_number:
-    print("Too low.")
-  else:
+  if(is_answer_correct(guess, answer)):
     game_over = True
-    print('You guessed correctly!')
   
-  if guesses_left <= 0 and not game_over:
+  if turns <= 0 and not game_over:
     print('You are out of guesses. You lose!')
     game_over = True
