5e88256b44e8179cb99b806d224e0386807799b1
Vladimir Baraniuk <vladimirbaraniuk@gmail.com>
2025-10-27 16:42:12 +0200

    refactor: naming

--- 11-20/Day_14_Higher_Lower/main.py
+++ 11-20/Day_14_Higher_Lower/main.py
@@ -2,26 +2,26 @@ from art import logo, vs
 from game_data import data
 import random
 
-def check_and_compare (first_person, second_person, user_choice):
-  """Takes two persons to compare and the one the user chose.
+def check_and_compare (acc1, acc2, user_choice):
+  """Takes two accounts to compare and the one the user chose.
   Checks which one has more followers. Returns True if user choice has more followers, False otherwise"""
 
-  if first_person["follower_count"] > second_person["follower_count"]:
-    if first_person["name"] == user_choice["name"]:
+  if acc1["follower_count"] > acc2["follower_count"]:
+    if acc1["name"] == user_choice["name"]:
       return True
     else:
       return False
     
-  elif second_person["follower_count"] > first_person["follower_count"]:
-    if second_person["name"] == user_choice["name"]:
+  elif acc2["follower_count"] > acc1["follower_count"]:
+    if acc2["name"] == user_choice["name"]:
       return True
     else:
       return False
     
-def show_current_pair (person1, person2):
-  print(f"Compare A: {person1["name"]}, a {person1["description"]}, from {person1["country"]}.")
+def show_current_pair (acc1, acc2):
+  print(f"Compare A: {acc1["name"]}, a {acc1["description"]}, from {acc1["country"]}.")
   print(vs)
-  print(f"Against B: {person2["name"]}, a {person2["description"]}, from {person2["country"]}.")
+  print(f"Against B: {acc2["name"]}, a {acc2["description"]}, from {acc1["country"]}.")
 
 def get_answer ():
   """Asks a user to input either 'A' or 'B'. Returns 'a' or 'b'"""
@@ -34,24 +34,24 @@ def start():
   random.shuffle(data)
   game_is_over = False
   score = 0
-  first_item = data.pop()
+  acc1 = data.pop()
 
   print(logo)
   while not game_is_over:
-    second_item = data.pop()
+    acc2 = data.pop()
     user_choice = {}
-    show_current_pair(first_item, second_item)
+    show_current_pair(acc1, acc2)
 
     answer = get_answer()
     if answer == "a":
-      user_choice = first_item
+      user_choice = acc1
     else:
-      user_choice = second_item    
+      user_choice = acc2    
 
-    is_guess_right = check_and_compare(first_item, second_item, user_choice)
+    is_guess_right = check_and_compare(acc1, acc2, user_choice)
     if is_guess_right:
       score += 1
-      first_item = second_item
+      acc1 = acc2
       print('\n' * 50)
       print(logo)
       print(f"You're right! Current score: {score}")
