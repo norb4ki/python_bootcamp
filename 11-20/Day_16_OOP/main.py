a0364cb5f6864173f0eed61cd5d0c8c47cb9650c
Vladimir Baraniuk <vladimirbaraniuk@gmail.com>
2025-10-30 16:20:35 +0200

    feat: add oop ver coffee machine

---
+++ 11-20/Day_16_OOP/main.py
@@ -0,0 +1,22 @@
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+coffee_machine = CoffeeMaker()
+money_machine = MoneyMachine()
+menu = Menu()
+is_working = True
+
+while(is_working):
+  beverage_name = input('What beverage would you like? (espresso/latte/cappucino)\n')
+  if beverage_name == 'off':
+    is_working = False
+  elif beverage_name == 'report':
+    coffee_machine.report()
+  else: 
+    beverage = menu.find_drink(beverage_name)
+    if beverage and coffee_machine.is_resource_sufficient(beverage):
+      if money_machine.make_payment(beverage.cost):
+        coffee_machine.make_coffee(beverage)
+
+
