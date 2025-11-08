ca59f43f2c0fbcf47dbd65984b4534a936b3d425
Vladimir Baraniuk <vladimirbaraniuk@gmail.com>
2025-11-08 12:27:00 +0200

    feat: add snake control

--- 11-20/day-20-snake-game/Snake.py
+++ 11-20/day-20-snake-game/Snake.py
@@ -1,39 +1,54 @@
 from turtle import Turtle
 
-directions = {
+DIRECTIONS = {
   "right": 0,
-  "down": 90,
+  "up": 90,
   "left": 180,
-  "up": 270 
+  "down": 270 
 }
+STARTING_POSITIONS = [(0,0), (-20, 0), (-40, 0)]
 
 class Snake():
   def __init__(self):
-    body_part = Turtle()
-    self.heading = directions["right"]
-    self.body = []
+    self.body = self.create_snake()
+
+  def create_snake(self):
+    segments = []
+    for position in STARTING_POSITIONS:
+      segment = Turtle('square')
+      segment.color('white')
+      segment.penup()
+      segment.goto(position)
+      segments.append(segment)
+    return segments
 
   def turn_right(self):
     # ignores backward turn attempt
-    if self.heading == directions["left"]:
+    if self.body[0].heading() == DIRECTIONS["left"]:
       return
-    self.heading = directions["right"]
+    self.body[0].setheading(DIRECTIONS["right"])
 
   def turn_left(self):
     # ignores backward turn attempt
-    if self.heading == directions["up"]:
+    if self.body[0].heading() == DIRECTIONS["right"]:
       return
-    self.heading = directions["down"]
+    self.body[0].setheading(DIRECTIONS["left"])
 
   def turn_up(self):
     # ignores backward turn attempt
-    if self.heading == directions["right"]:
+    if self.body[0].heading() == DIRECTIONS["down"]:
       return
-    self.heading = directions["left"]
+    self.body[0].setheading(DIRECTIONS["up"])    
 
   def turn_down(self):
     # ignores backward turn attempt
-    if self.heading == directions["down"]:
+    if self.body[0].heading() == DIRECTIONS["up"]:
       return
-    self.heading = directions["up"]
+    self.body[0].setheading(DIRECTIONS["down"])
   
+  def move(self):
+    for body_part in range(len(self.body) - 1, 0, -1):
+      new_x = self.body[body_part - 1].xcor()
+      new_y = self.body[body_part - 1].ycor()
+      self.body[body_part].goto(new_x, new_y)
+    self.body[0].forward(20)
--- 11-20/day-20-snake-game/main.py
+++ 11-20/day-20-snake-game/main.py
@@ -1,35 +1,23 @@
 from turtle import Screen, Turtle
-import time 
+import time
+from Snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title('Snake')
 screen.tracer(0)
+snake = Snake()
+screen.onkey(snake.turn_down, 'Down')
+screen.onkey(snake.turn_up, 'Up')
+screen.onkey(snake.turn_right, 'Right')
+screen.onkey(snake.turn_left, 'Left')
+screen.listen()
 
-segments = []
-game_is_on = True
-starting_positions = [(0,0), (-20, 0), (-40, 0)]
-
-for position in starting_positions:
-  segment = Turtle('square')
-  segment.color('white')
-  segment.penup()
-  segment.goto(position)
-  segments.append(segment)
-
-while game_is_on:
+def game_loop():
+  snake.move()
   screen.update()
-  time.sleep(1)
+  screen.ontimer(game_loop, 100)  # move every 100 ms (~10 FPS)
 
-  for seg_num in range(len(segments) - 1, 0, -1):
-    new_x = segments[seg_num - 1].xcor()
-    new_y = segments[seg_num - 1].ycor()
-    segments[seg_num].goto(new_x, new_y)
-  segments[0].forward(20)
-
-
-
-
-
-screen.exitonclick()
+game_loop()  # start the loop
+screen.mainloop()
