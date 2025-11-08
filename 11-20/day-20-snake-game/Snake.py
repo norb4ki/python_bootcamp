291500ef64276e0e3d83d6276b2d4f552174c9aa
Vladimir Baraniuk <vladimirbaraniuk@gmail.com>
2025-11-08 12:36:51 +0200

    refactor: constants

--- 11-20/day-20-snake-game/Snake.py
+++ 11-20/day-20-snake-game/Snake.py
@@ -7,10 +7,12 @@ DIRECTIONS = {
   "down": 270 
 }
 STARTING_POSITIONS = [(0,0), (-20, 0), (-40, 0)]
+STEP = 20
 
 class Snake():
   def __init__(self):
     self.body = self.create_snake()
+    self.head = self.body[0]
     self.turn_applied = True
 
   def create_snake(self):
@@ -25,33 +27,33 @@ class Snake():
 
   def turn_right(self):
     # ignores backward turn attempt
-    if self.body[0].heading() == DIRECTIONS["left"] or not self.turn_applied:
+    if self.head.heading() == DIRECTIONS["left"] or not self.turn_applied:
       return
-    self.body[0].setheading(DIRECTIONS["right"])
+    self.head.setheading(DIRECTIONS["right"])
     self.turn_applied = False
 
   def turn_left(self):
     # ignores backward turn attempt
-    if self.body[0].heading() == DIRECTIONS["right"] or not self.turn_applied:
+    if self.head.heading() == DIRECTIONS["right"] or not self.turn_applied:
       return
-    self.body[0].setheading(DIRECTIONS["left"])
+    self.head.setheading(DIRECTIONS["left"])
 
   def turn_up(self):
     # ignores backward turn attempt
-    if self.body[0].heading() == DIRECTIONS["down"] or not self.turn_applied:
+    if self.head.heading() == DIRECTIONS["down"] or not self.turn_applied:
       return
-    self.body[0].setheading(DIRECTIONS["up"])    
+    self.head.setheading(DIRECTIONS["up"])    
 
   def turn_down(self):
     # ignores backward turn attempt
-    if self.body[0].heading() == DIRECTIONS["up"]  or not self.turn_applied:
+    if self.head.heading() == DIRECTIONS["up"]  or not self.turn_applied:
       return
-    self.body[0].setheading(DIRECTIONS["down"])
+    self.head.setheading(DIRECTIONS["down"])
   
   def move(self):
     for body_part in range(len(self.body) - 1, 0, -1):
       new_x = self.body[body_part - 1].xcor()
       new_y = self.body[body_part - 1].ycor()
       self.body[body_part].goto(new_x, new_y)
-    self.body[0].forward(20)
+    self.head.forward(STEP)
     self.turn_applied = True
