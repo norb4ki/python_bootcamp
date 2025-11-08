1d5e44ae8b95fd69bb3d6aa8eb0c52655052ee82
Vladimir Baraniuk <vladimirbaraniuk@gmail.com>
2025-11-08 12:33:08 +0200

    fix: exclude backward turn

--- 11-20/day-20-snake-game/Snake.py
+++ 11-20/day-20-snake-game/Snake.py
@@ -11,6 +11,7 @@ STARTING_POSITIONS = [(0,0), (-20, 0), (-40, 0)]
 class Snake():
   def __init__(self):
     self.body = self.create_snake()
+    self.turn_applied = True
 
   def create_snake(self):
     segments = []
@@ -24,25 +25,26 @@ class Snake():
 
   def turn_right(self):
     # ignores backward turn attempt
-    if self.body[0].heading() == DIRECTIONS["left"]:
+    if self.body[0].heading() == DIRECTIONS["left"] or not self.turn_applied:
       return
     self.body[0].setheading(DIRECTIONS["right"])
+    self.turn_applied = False
 
   def turn_left(self):
     # ignores backward turn attempt
-    if self.body[0].heading() == DIRECTIONS["right"]:
+    if self.body[0].heading() == DIRECTIONS["right"] or not self.turn_applied:
       return
     self.body[0].setheading(DIRECTIONS["left"])
 
   def turn_up(self):
     # ignores backward turn attempt
-    if self.body[0].heading() == DIRECTIONS["down"]:
+    if self.body[0].heading() == DIRECTIONS["down"] or not self.turn_applied:
       return
     self.body[0].setheading(DIRECTIONS["up"])    
 
   def turn_down(self):
     # ignores backward turn attempt
-    if self.body[0].heading() == DIRECTIONS["up"]:
+    if self.body[0].heading() == DIRECTIONS["up"]  or not self.turn_applied:
       return
     self.body[0].setheading(DIRECTIONS["down"])
   
@@ -52,3 +54,4 @@ class Snake():
       new_y = self.body[body_part - 1].ycor()
       self.body[body_part].goto(new_x, new_y)
     self.body[0].forward(20)
+    self.turn_applied = True
