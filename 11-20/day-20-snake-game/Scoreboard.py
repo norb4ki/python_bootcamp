cee798780d94839b5c814d918af19d566d9e3f50
Vladimir Baraniuk <vladimirbaraniuk@gmail.com>
2026-02-06 17:23:21 +0200

    feat: read and write high score to file

--- 11-20/day-20-snake-game/Scoreboard.py
+++ 11-20/day-20-snake-game/Scoreboard.py
@@ -8,7 +8,8 @@ class Scoreboard(Turtle):
   def __init__(self):
     super().__init__()
     self.penup()
-    self.high_score = 0
+    with open("high_score.txt", 'r') as f:
+      self.high_score = int(f.read())
     self.score = 0
     self.hideturtle()
     self.teleport(0, 260)
@@ -18,11 +19,13 @@ class Scoreboard(Turtle):
   def increase_score(self):
     self.clear()
     self.score += 1
-    if self.score > self.high_score:
-      self.high_score = self.score
     self.write(f'Score: {self.score} High score: {self.high_score}', align=ALIGNMENT, font=FONT_REGULAR )
 
   def game_over(self):
+    if self.score > self.high_score:
+      self.high_score = self.score
+      with open("high_score.txt", 'w') as f:
+        f.write(f'{self.high_score}')
     self.clear()
     self.teleport(0, 40)
     self.write(f'GAME OVER', align=ALIGNMENT, font=FONT_BIG )
@@ -31,8 +34,6 @@ class Scoreboard(Turtle):
   
   def reset(self):
     self.teleport(0, 260)
-    if self.score > self.high_score:
-      self.high_score = self.score
     self.score = 0
     self.clear()
     self.write(f'Score: {self.score} High score: {self.high_score}', align=ALIGNMENT, font=FONT_REGULAR )
